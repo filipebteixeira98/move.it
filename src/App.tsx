9cc8365575e1d353ab898a8fbeefed2e7efba89f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-17 00:48:20 -0300

    chore: added div element & container styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import './styles/global.css';
 
 function App() {
   return (
-    <>
-      <h1>Move.it</h1>
+    <div className="container">
       <ExperienceBar />
-    </>
+    </div>
   );
 }
 

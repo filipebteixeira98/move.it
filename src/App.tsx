b03f5a6bd8e7fecaa79e825577ee4417dd5070f9
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-16 23:56:24 -0300

    chore: created experience bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ExperienceBar } from './components/ExperienceBar';
+
 import './styles/global.css';
 
 function App() {
-  return <h1>Move.it</h1>;
+  return (
+    <>
+      <h1>Move.it</h1>
+      <ExperienceBar />
+    </>
+  );
 }
 
 export default App;

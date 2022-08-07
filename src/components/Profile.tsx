7ff0ca36ad4b7608ca07a5f09f2507ef8f03295f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-08-07 02:47:58 -0300

    chore: created profile component structure

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+export function Profile() {
+  return (
+    <div>
+      <img
+        src="https://github.com/filipebteixeira98.png"
+        alt="Filipe Teixeira"
+      />
+      <div>
+        <strong>Filipe Teixeira</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
